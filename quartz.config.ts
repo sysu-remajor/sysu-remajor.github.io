6a9c2bbe937ff60e028a0846a6444d4bd0aaa0d0
Candlest <candlest1112@gmail.com>
2026-02-24 23:02:45 +0800

    vault backup: 2026-02-24 23:02:45

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "",
+    pageTitle: "SYSU 转专业手册",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
